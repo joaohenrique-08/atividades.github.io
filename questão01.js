20d8c0fd14858a8e0c13dc2c2b118a94c1208c69
João henrique <jh0200768@gmail.com>
2025-04-25 20:11:16 +0000

    atividade 1

---
+++ questão01.js
@@ -0,0 +1,16 @@
+let n1 = 3;
+let n2 = 3;
+let n3 = 5;
+let n4 = 5;
+let media = (n1+n2+n3+n4)/4;
+
+if (media >= 6){
+    console.log("aluno aprovado");
+    
+}
+else if(media < 4){
+    console.log("aluno reprovado");
+}
+else{
+    console.log("aluno de recuperação");
+}
