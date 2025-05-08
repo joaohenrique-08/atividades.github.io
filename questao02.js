3b09d10a763f59ce473fcb420f609f64862a91f6
João henrique <jh0200768@gmail.com>
2025-05-08 02:07:38 +0000

    atividade 01

--- questao02.js
+++ questao02.js
@@ -1,3 +1,27 @@
 let idade = 10
 
+if (idade <= 3){
+    console.log("é um bebê");    
+}
+
+else if(idade > 3 & idade <= 11){
+    console.log("é uma criança");    
+}
+
+else if(idade > 11 & idade <= 14){
+    console.log("é um pré adolecente");    
+}
+
+else if(idade > 14 & idade <= 17){
+    console.log("é uma adolecente");    
+}
+
+else if(idade > 17 & idade <= 64){
+    console.log("é uma criança");    
+}
+
+else{
+    console.log("é um idoso");  
+}
+
 
---
+++ questão04.js
@@ -0,0 +1,9 @@
+let salario = 15000
+let emprestimo = 2999
+
+if (emprestimo <= (20/100)*salario){
+    console.log("emprestimo concedido");
+}
+else{
+    console.log("emprestimo não concedido");
+}
