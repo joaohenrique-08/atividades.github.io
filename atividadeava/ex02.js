03e8e8742332993fd0dc9774878b0f5b9916d416
João henrique <jh0200768@gmail.com>
2025-06-06 13:08:17 +0000

    prova

--- atividadeava/ex02.js
+++ atividadeava/ex02.js
@@ -1,12 +1,10 @@
 function simularPoupanca(valorMensal, taxaJuros, meses) {
-
     let total = 0;
+    let jurosmensal = taxaJuros/100
 
-    for (let i = 0; i <= meses; i++) {
-        total += valorMensal;         
-        total *=(1 + taxaJuros / 100); 
-    }
-    
+    for (let i = 1; i <= meses; i++) {
+        total = (total + valorMensal)*( 1 + jurosmensal); 
+    }  
     console.log(total.toFixed(2));
 }
 simularPoupanca(1000,0.48, 18);
